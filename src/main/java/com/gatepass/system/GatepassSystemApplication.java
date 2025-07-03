a4e93082b77a73f8324594a32f33bc9b20c1220a
Lahiru Jayawardhane <93923625+lahirujay99@users.noreply.github.com>
2025-07-03 14:06:24 +0530

    initial commit

---
+++ src/main/java/com/gatepass/system/GatepassSystemApplication.java
@@ -0,0 +1,13 @@
+package com.gatepass.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatepassSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatepassSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gatepass/system/GatepassSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gatepass.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatepassSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
